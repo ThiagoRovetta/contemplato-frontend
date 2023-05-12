ea871934334c809b9d7c558c589f0b3ca1130f6d
ThiagoRovetta <rovetta.thiago@gmail.com>
2023-05-12 18:56:31 -0300

    Fixed condition to redirect to login page.

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -5,7 +5,7 @@ export const api = axios.create({
 });
 
 api.interceptors.response.use((response) => response, (error) => {
-  if (error?.response?.status === 401) {
+  if (error?.response?.status === 401 && window.location.pathname !== '/') {
     localStorage.removeItem('token');
     localStorage.removeItem('currentUser');
     window.location.href = '/';
